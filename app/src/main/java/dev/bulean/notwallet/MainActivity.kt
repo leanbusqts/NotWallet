bbf62521e5c4d46c9edd81474702040a71ddb7bd
Leandro Busquets <leandro.busquets@mercadolibre.com>
2022-04-14 10:25:30 -0300

    first commit

---
+++ app/src/main/java/dev/bulean/notwallet/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.bulean.notwallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
